cf45727f5123e38115cbcc91aa222cf78044c6e8
Chris Franklin <cf73021@gmail.com>
2018-08-27 20:27:12 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,28 @@
 class Triangle
-  
+  attr_accessor :left, :right, :middle
 
+  def initialize(left, right, middle)
+    @left = left
+    @right = right
+    @middle = middle
+  end
+
+  def kind
+    if left == 0 || right == 0 || middle == 0 || left < 0 || right < 0 || middle < 0 || (left + right) <= middle || (right + middle) <= left || (left + middle) <= right
+      raise TriangleError
+    elsif left == right && right == middle
+      return :equilateral
+    elsif left != right && right == middle || left == right && right != middle || left == middle && right != middle
+      return :isosceles
+    elsif left != right && right != middle
+      return :scalene
+    end
+  end
 
   class TriangleError < StandardError
-
+    def message
+      "This is not a triangle!"
+    end
   end
+
 end
