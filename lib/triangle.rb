e2b2fdfe399e85d19bbfd0dda280bcc7c74251b4
Chris Franklin <cf73021@gmail.com>
2018-08-27 20:24:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if left == 0 || right == 0 || middle == 0 || left < 0
+    if left == 0 || right == 0 || middle == 0 || left < 0 || right < 0 || middle < 0
       raise TriangleError
     elsif left == right && right == middle
       return :equilateral
